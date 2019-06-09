d9f218b4572c10919cd13d867ad1f20818c8c52f
github username <github email address>
2019-06-09 09:50:47 +0100

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,18 @@
+class Appointment
+
+    @@all = []
+
+    attr_accessor :patient, :doctor
+
+    def initialize(date, patient, doctor)
+        @@all << self
+        @date = date
+        @patient = patient
+        @doctor = doctor
+    end
+
+    def self.all
+        @@all
+    end
+
+end
---
+++ lib/run.rb
@@ -0,0 +1,9 @@
+require_relative "doctor.rb"
+require_relative "patient.rb"
+require_relative "appointment.rb"
+
+require "pry"
+
+binding.pry
+
+#
